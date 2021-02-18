d807cba06da1682f16e84f9b65e0fbab0a92463e
third <iringanremigio@gmail.com>
2021-02-18 22:39:56 +0800

    set up mongoose connection

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const mongoose = require("mongoose");
+
+const File = require("./model");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("Database connected");
+});
+
 const app = express();
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("index");
+});
+
+app.post("/", (req, res) => {
+	const file = new File(req.body);
+	file.save();
+	res.redirect("/");
 });
 
 app.listen("3000", () => {
