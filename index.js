6a42630c98ac39e4923c9a8313b3434f0d647573
third <iringanremigio@gmail.com>
2021-02-19 21:55:53 +0800

    setup dotenv

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
 const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
@@ -26,7 +30,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.render("index");
+	res.send(process.env.TEST);
 });
 
 app.post("/", (req, res) => {
